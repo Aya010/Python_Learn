57343cccf1455c283fc90eeb16792c5eef969092
Aya010 <a1178608567@qq.com>
2020-07-21 00:47:30 +0800

    修改多态相关的内容

--- reader.py
+++ reader.py
@@ -1,88 +1,120 @@
 import csv  # python标准库
-
+import os.path
 import zipfile  # 引用第三方外来库
 
 import person
+from person import Person
 # import person # 本工程内写的
 
 
-class TXTReader():  # return a list
+class Reader:
+    ext = None
+
+    def __init__(self, file_name):
+        self._file = file_name
+        suffix = os.path.splitext(file_name)[1]
+        for x in Reader.__subclasses__():
+            if suffix == x.ext:
+                self._func = x(file_name)
+
+    def read(self,  people_list):
+        return self._func.read(people_list)
+
+
+class TxtReader(Reader):
+    ext = ".txt"
+
     def __init__(self, txt_file):
         self.file_name = txt_file
 
-    def read(self, ret_list):
+    def read(self, people_list):
         with open(self.file_name) as txt_file:
             for line in txt_file:
                 line_words = line.split(" ")
-                person_ = person.Person(line_words[0], eval(line_words[1]), eval(line_words[2]))
-                ret_list.append(person_)
-        return ret_list
-    def __iter__(self): # save data
+                person_ = person.Person(line_words[0], eval(
+                    line_words[1]), eval(line_words[2]))
+                people_list.append(person_)
+        return people_list
+
+    def __iter__(self):
         with open(self.file_name) as txt_file:
-            line = txt_file.readline()
-            while True:
-                if not line:
-                    break
+            for line in txt_file:
                 line_words = line.split(" ")
-                person_ = person.Person(line_words[0], eval(line_words[1]), eval(line_words[2]))
+                person_ = person.Person(line_words[0], eval(
+                    line_words[1]), eval(line_words[2]))
                 yield person_
 
-class CSVReader():  # return a list
+
+class CsvReader(Reader):  # return a list
+    ext = '.csv'
+
     def __init__(self, csv_file):
         self.file_name = csv_file
 
-    def read(self, ret_list):
+    def read(self, people_list):
         with open(self.file_name, 'r') as csvfile:
             lines = csv.reader(csvfile, delimiter=' ')
             for line in lines:
                 line_words = line[0].split(",")
-                person_ = person.Person(line_words[0], eval(line_words[1]), eval(line_words[2]))
-                ret_list.append(person_)
-        return ret_list
+                person_ = person.Person(line_words[0], eval(
+                    line_words[1]), eval(line_words[2]))
+                people_list.append(person_)
+        return people_list
+
+    def __iter__(self):
+        with open(self.file_name) as csv_file:
+            for line in csv_file:
+                line_words = line.split(",")
+                person_ = person.Person(line_words[0], eval(
+                    line_words[1]), eval(line_words[2]))
+                yield person_
 
 
-class ZIPReader():  # return a list
+class ZipReader(Reader):  # return a list
+    ext = '.zip'
+
     def __init__(self, zip_file):
         self.file_name = zip_file
-        self._txt = '.txt'
-        #self._csv = '.csv'
-    def read(self, ret_list):
-        with zipfile.ZipFile(self.file_name,'r') as ZipReader:
-            file_list = ZipReader.namelist()
+
+    def read(self, people_list):
+        with zipfile.ZipFile(self.file_name, 'r') as zip_reader:
+            file_list = zip_reader.namelist()
             for file_ in file_list:
-                read_file = ZipReader.read(file_).decode('UTF-8')
+                read_file = zip_reader.read(file_).decode('UTF-8')
                 read_lines = read_file.split('\r\n')
-                if self._txt in file_:
-                    for read_line in read_lines:
-                        read_word = read_line.split(' ')
-                        read_word = read_word[:3]
-                        if read_word != ['']:
-                            person_ = person.Person(read_word[0], eval(read_word[1]), eval(read_word[2]))
-                            ret_list.append(person_)
-                else:
-                    for read_line in read_lines:
-                        read_word = read_line.split(',')
-                        if read_word != ['']:
-                            person_ = person.Person(read_word[0], eval(read_word[1]), eval(read_word[2]))
-                            ret_list.append(person_)
-        return ret_list
+                for x in ZipReader.__subclasses__():
+                    if file_[-4:] == x.ext:
+                        person_read = x(read_lines)
+                        people_list = person_read.read(people_list)
+        return people_list
 
 
-class Reader():
-    def __init__(self, file_name):
-        txt_ = ".txt"
-        csv_ = ".csv"
-        zip_ = ".zip"
-        #self._file = file_name
-        if txt_ in file_name:
-            self._func = TXTReader(file_name)
-        elif csv_ in file_name:
-            self._func = CSVReader(file_name)
-        else:
-            self._func = ZIPReader(file_name)
-    def read(self, ret_list):
-        return self._func.read(ret_list)
+class ZipReaderTxt(ZipReader):
+    ext = '.txt'
+    def __init__(self, read_lines):
+        self.read_lines = read_lines
+    def read(self, people_list):
+        for read_line in self.read_lines:
+            read_word = read_line.split(' ')
+            read_word = read_word[:3]
+            if read_word != ['']:
+                person_info = person.Person(read_word[0], eval(
+                    read_word[1]), eval(read_word[2]))
+                people_list.append(person_info)
+        return people_list
 
+
+class ZipReaderCsv(ZipReader):
+    ext = '.csv'
+    def __init__(self, read_lines):
+        self.read_lines = read_lines
+    def read(self, people_list):
+        for read_line in self.read_lines:
+            read_word = read_line.split(',')
+            if read_word != ['']:
+                person_info = person.Person(read_word[0], eval(read_word[1]), eval(read_word[2]))
+                people_list.append(person_info)
+        return people_list
 
 """    def read(self):
         with zipfile.ZipFile(self.file_name, 'r') as ZipReader:
@@ -93,38 +125,29 @@ class Reader():
                     txt_read = TXTReader(file_)
                     txt_ring = txt_read.read()
                     for i in txt_ring:
-                        self.ret_list.append(i)
+                        self.people_list.append(i)
                 else:
                     csv_read = CSVReader(file_)
                     csv_ring = csv_read.read()
                     for i in csv_ring:
-                        self.ret_list.append(i)
-        return self.ret_list
+                        self.people_list.append(i)
+        return self.people_list
 """
 
 
-
 if __name__ == "__main__":
+    ring_txt = []
+    func = Reader("josephus_list2.txt")
+    ring_txt = func.read(ring_txt)
+    assert(ring_txt[0].get_age() == 11)
 
-    """    flag = 0
-        if flag == 1:
-            ring = TXTReader("josephus_list2.txt")
-            ring = ring.read()
+    ring_csv = []
+    func = Reader("josephus_list1.csv")
+    ring_csv = func.read(ring_csv)
+    assert(ring_csv[0].get_age() == 0)
 
-        elif flag == 2:
-            ring = CSVReader("josephus_list1.csv")
-            ring = ring.read()
-
-        else:
-            ring = ZIPReader("josephus.zip")
-            ring = ring.read()
-            
-        for i in ring:
-            print(i)
-    """
-    ring = []
-    file_name = "josephus_list2.txt"
-    func = TXTReader(file_name)
-    #ring = func.read(ring)
-    for i in func:
-        print(i)
+    ring_zip = []
+    func = Reader("josephus.zip")
+    ring_zip = func.read(ring_zip)
+    assert(ring_zip[0].get_age() == 11)
+    assert(ring_zip[10].get_age() == 0)
